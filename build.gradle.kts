4471c34c5c32686e83d91d6bcc5b8a80da4e8c85
Risa Labs <enterprise@risalabs.ai>
2026-09-23 22:17:06 +0000

    🔖 Bump version to 0.2.2 [skip ci]

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 }
 
 group = "ai.rever.boss.plugin.dynamic"
-version = "0.2.1"
+version = "0.2.2"
 
 java { toolchain { languageVersion.set(JavaLanguageVersion.of(17)) } }
 kotlin { compilerOptions { jvmTarget.set(JvmTarget.JVM_17) } }
